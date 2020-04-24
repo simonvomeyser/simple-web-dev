659b404ca3eba53449e8e83c4c0e0205f4720cfc
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-04-24 17:10:18 +0200

    Create really basic proof of concept
    Blade files can be used as blog posts!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <p>Welcome, here are your blade posts</p>
     @foreach ($posts as $item)
-        {{$item->title}}
+        <a href="{{route('posts.single', $item->slug)}}">{{$item->title}}</a> <br><br>
     @endforeach
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Illuminate\View\Factory;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\FileViewFinder;
 use Illuminate\View\View;
 
 /*
@@ -18,12 +20,15 @@ use Illuminate\View\View;
 */
 $files = File::allFiles(resource_path('views/posts'));
 
-$posts = collect();
+// dd(app(FileViewFinder::class, ['paths' => [resource_path('views').'/posts']]));
+$posts = new Collection();
 foreach ($files as $key => $value) {
-    $view = view('posts.' . Str::replaceLast('.blade', '', $value->getFilenameWithoutExtension()));
+    $viewName = Str::before($value->getFilenameWithoutExtension(), '.blade');
+    $view = view("posts.$viewName");
     $data = $view->renderSections();
     $post = new stdClass();
     $post->title = $data['title'] ?? '';
+    $post->slug = $viewName;
     $post->excerpt = $data['excerpt'] ?? '';
     $post->date = $data['date'] ?? '';
     $posts->add($post);
@@ -31,5 +36,12 @@ foreach ($files as $key => $value) {
 
 
 Route::get('/', function () use ($posts){
-    return view('welcome')->with(compact('posts'));
-});
+    return view('welcome')->with("posts" , $posts->only(0,1));
+})->name('index');
+
+Route::get('posts/{slug}', function($slug) {
+
+    // Later: Posts::findOrFail($slug)
+
+    return view("posts.$slug");
+})->name('posts.single');
