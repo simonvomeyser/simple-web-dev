0d369541dee8547ad583ca2c158d99fdcabf573d
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-04-25 10:04:22 +0200

    Create app test

--- tests/Feature/AppTest.php
+++ tests/Feature/AppTest.php
@@ -5,14 +5,10 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class AppTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    /** @test */
+    public function the_app_is_reachable()
     {
         $response = $this->get('/');
 
