5ffb93e78d9f80c993cce019115294df10e28f92
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-04-25 10:06:09 +0200

    Set config in parent class, prepare cleanup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,27 @@
 
 namespace Tests;
 
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+
+        Config::set('posts.location', base_path('tests/Fixtures/posts'));
+
+        // Cleanup posts folder    
+    }
+    protected function tearDown() : void
+    {
+        parent::tearDown();
+        
+        // Cleanup posts folder    
+    }
 }
