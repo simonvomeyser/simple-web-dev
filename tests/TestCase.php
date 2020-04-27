55a744e66cb31e41d03653bdb0c9dc39fc79ee28
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-04-27 16:23:24 +0200

    Remove setting config again
    Do that beforehand

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,19 +11,18 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
 
 
-    protected function setUp() : void
+    protected function setUp(): void
     {
         parent::setUp();
 
         Config::set('posts.location', base_path('tests/Fixtures/posts'));
-        Config::set('view.paths', array_merge(config('view.paths'), [base_path('tests/Fixtures')]));
 
         // Cleanup posts folder    
     }
-    protected function tearDown() : void
+    protected function tearDown(): void
     {
         parent::tearDown();
-        
+
         // Cleanup posts folder    
     }
 }
