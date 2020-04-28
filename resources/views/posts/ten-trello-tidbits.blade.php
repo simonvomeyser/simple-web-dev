c701e48cf9f7b6776752b5f3bcfbf535169afd31
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-04-28 12:03:09 +0200

    Add first blogpost

---
+++ resources/views/posts/ten-trello-tidbits.blade.php
@@ -0,0 +1,257 @@
+@extends('layouts.post')
+
+@section('title', 'Ten Trello Tidbits or Why Their UX Team Deserves a Hug ')
+
+@section('release-date', '')
+
+@section('slugs', '[]')
+
+@section('excerpt', '')
+
+@section('tags', '[]')
+
+@section('header_image', 'https://placehold.it/1024x768')
+
+@section('list_header_image', 'https://placehold.it/1024x768')
+
+@section('content')
+
+I am receiving way to many E-mails with subjects like “Re: Re: Re: The logo should be more blue-ish”. This made facepalm
+a lot, but also try at a lot of different project management tools. A comparison between a few of them should be the
+topic of another post, but I want to mention a few things that <a href="http://trello.com/">Trello</a> in
+particular does really well and <strong>with an incredible amount of focus on User Experience (UX)</strong>.
+
+I mean.. I regularly feel hugged by the trello UX team.. is that really just me?
+
+<img src="https://res.cloudinary.com/simonvomeyser/image/upload/v1522257594/Trello/agkalini3.jpg">
+
+Ermm, nevermind.. presenting:
+
+<blockquote>
+    Ten Trello Tidbits or Why Their UX Team Deserves a Hug
+</blockquote>
+
+Trello features a “Board has many Lists has many Cards” concept and therefore is really flexible, but no
+unique snowflake. Cards can contain comments, media, descriptions, todo lists and due dates. Nothing fancy,
+but it’s the details I want to draw your attention to.
+
+<h2>The Batch create Card and Todo feature</h2>
+One of the things I more or less noticed before reading about it is a what I would call a “batch create”
+feature.
+
+You can literally drop a bunch of text into the “Add todolist item” input field and Trello will create a new
+item with every line break. Woha! The same thing works with the creation of cards, though you will be asked
+beforehand. This is a feature that has recently landed in some todo applications (Check out <a
+    href="https://todoist.com">Todoist</a> my app of choice) but Trello is the place I saw it first.
+
+<figure
+    class="wp-block-embed-youtube wp-block-embed is-type-video is-provider-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+    <div class="wp-block-embed__wrapper">
+        <iframe title="Batch Create Todos and Cards in Trello"
+            src="https://www.youtube.com/embed/t9rC4klx7o0?feature=oembed" frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+    </div>
+</figure>
+
+<h2>Intuitive keyboard navigation</h2>
+I know that I am a little too much into shortcuts. I admit it. I spend hours learning shortcuts that will
+maybe save me 5 Minutes a year. But the good thing about Trello’s keyboard navigation is: There is hardly
+anything to learn. Want to navigate “Down” ? Press “Arrow Down”. Want navigate right? Do a handstand and
+whistle “La Cucaracha” … no, just press “Right Arrow”.
+
+I know this stuff sounds simple… but if it’s so simple why do most tools neglect that? Why can’t I
+navigate my cards or tasks in other applications with the keyboard arrows and check todos or open an issue
+detail view with Enter or Space? Okay, let’s be a little bit more helpful than grumpy:
+
+<ul>
+    <li>N New Card</li>
+    <li>D Set Due Date</li>
+    <li>E (Quick) Edit</li>
+    <li>F to search a Card</li>
+</ul>
+Shortcuts can be so easy. Check Trellos <a href="https://trello.com/shortcuts">Shortcut Page</a> for more
+info, you’ll might be surprised.
+
+Since the whole point of this article is to mention this little UX gems: Pressing “Right Arrow” does not even
+take you to the list right of you, but also to the corresponding card you were in the current list. I can’t
+be the only one who finds this attention to detail impressive.
+
+<img src="https://res.cloudinary.com/simonvomeyser/image/upload/v1531908826/Trello/trello-navigate-adjacent-cell.gif">
+<h2>Notifications and fighting E-Mail SPAM</h2>
+Most Applications either inform you too seldomly about important changes or bombard you with emails by
+default. I have seen coworkers simple marking all emails coming from a tool as spam since it was too
+complicated to find the settings in the app itself. At Trello, the team really has put a lot of thought into
+their notification and email system to not make them loose significance by sheer mass.
+
+There are three simple options for E-Mails:
+
+<ul>
+    <li>Send me E-Mails as fast as possible</li>
+    <li>Send them once per Hour</li>
+    <li>Don’t send me E-Mails at all</li>
+</ul>
+There are no “E-Mail Schemes”, no “call your admin to change this”, no nothing overly complex for normal
+users to understand. And I know that some things seem obvious for somebody working in webdevelopment, but
+nearly all projects involve other stakeholders without our background so the tool should be as easy to use
+as humanly possible. Keep one thing in mind:
+
+<blockquote>
+    Even the most sophisticated tool will loose all it’s power when not understood and ignored by parts of
+    the team.
+
+</blockquote>
+The emails are only send when you are “notified” inside of Trello. It is therefore worth to mention,
+that the notification system absolutely makes sense. You’ll get notified if:
+
+<ul>
+    <li>You are mentioned</li>
+    <li>You are watching a cards (issues) activity</li>
+    <li>You are watching the whole board (project)</li>
+</ul>
+The notifications don’t jump into your face, they are hidden behind the “Bell Symbol” right next to your
+accounts profile pitcture .
+
+Think how much thought has been put into that, and now it looks so simple and obvious. That’s when you know a
+system is good.
+
+<h2>Flexibility and easy to learn for everyone</h2>
+Needless to say, flexibility is one of the biggest strengths Trello has. The “Board List Card” principle can
+be used to represent almost anything. Most commonly though, it is used in a <a
+    href="https://en.wikipedia.org/wiki/Scrum_(software_development)">Scrum</a> or <a
+    href="https://en.wikipedia.org/wiki/Kanban">Kanban</a>-ish way, where the lists represent things like
+“todo” “doing” or “backlog”.
+
+I’ve also seen people use (and myself used) Trello for the wildest things. An example is using the lists for
+every weekday and the cards for things they to do. Another <a
+    href="https://blog.hubspot.com/sales/personal-connections-template">example</a> is to create cards for
+to organize the people in your network to improve relationships with them or use Lists for every member of
+your team to organize their todos.
+
+The possibilities are <a href="https://trello.com/inspiration">endless</a>, and this is why I am a big fan of
+advising teams during consulting sessions to (first) use trello, move fast, try things and find their very
+own workflow. This is way more important than any tool, and Trello’s habit of not getting into your way
+helps you immensely. After all:
+
+<blockquote>
+    When you find the right process, the tool you use will become insignificant.
+
+</blockquote>
+<h2>Create cards via E-Mail</h2>
+A nice feature is the fact, that every board comes with a dedicated email address. When you are poor person
+in charge of client communication, you can simply send a task from a conversation to a board. From there
+your team internal workflow should pick up.
+
+You can find the feature under the Menu on the right and then clicking the “more” link and then
+“Email-to-board-settings”.
+
+<img
+    src="https://res.cloudinary.com/simonvomeyser/image/upload/v1526544451/Trello/Bildschirmfoto_2018-05-17_um_10.06.55.png">
+
+And even if I repeat myself: I am not saying this feature is perfect, it becomes quite confusing at times to
+have a long blob of text in there. I am just saying that the idea is really good and it is cool how
+much energy and thought has been put into things like this.
+
+<h2>Speed &amp; Background Saves</h2>
+There is not even the slightest indicator of loading time. I’ve worked with the <a href="https://developers.trello.com/"
+    target="_blank" rel="noopener noreferrer">Trello REST
+    API</a> and I know that it is not the fasted interface ever invented. What I am trying to stress
+here is, that the <strong>perceived performance </strong>is incredible. When on a board, you almost
+don’t notice that your actions are saved in the background.
+
+Even if you don’t save your changes, drafts of comments or cards will be persisted so you don’t loose
+something accidentally. This approach can be found in many apps, but Trello gives me the most confidence
+that my changes will still be there.
+
+<h2>Smartphone Apps</h2>
+The UX of Smartphone Apps is a whole world of their own and I will not cover anything about it here. I simply
+want to mention, that I (as an user of the Android App) was absolutely happy with it. It’s even
+<strong>partly possible to work offline</strong> when you opened a board before and changes will be merged
+later. I must say, that I found the notifications a little annoying but that could be my personal
+preference since I tend to disable all notifications on my phone.
+
+<h2>Search and filter cards like a pro</h2>
+Something I grown to like more and more over the last month is the awesome search function. As a Developer, I
+love <a href="https://en.wikipedia.org/wiki/Approximate_string_matching"><em>fuzzy
+        search</em></a> working to different degrees in most text editors or IDEs. You just type
+something you know is contained in the File or Command you want, and voilà, the thing you look for and
+the closest other matches are found.
+
+The Trello team must have put some serious effort into their own search and filter function because it works
+quite similar. You can type words that are contained in a card, regardless of their order, case and board
+they are on. There is direct feedback that pops up, so you don’t have to push a button and wait just to
+notice, that you spelled something wrong.
+
+There are special commands you can use, all of them documented <a
+    href="https://help.trello.com/article/808-searching-for-cards-all-boards">here.</a> As an example,
+you can use @me to only show cards assigned to you.
+
+The last awesome thing to mention is the fact, that you can even chain things like a search phrase, the name
+of a board and the command to show only cards assigned to you. Combine this with keyboard shortcuts, and you
+have a cool workflow to find relevant cards.
+
+<img src="https://res.cloudinary.com/simonvomeyser/image/upload/v1532162435/Trello/Kapture_2018-07-21_at_10.39.57.gif">
+It’s worth to mention, that <em>search </em>is intended for a search across all
+boards, <em>filtering </em>works only in the current board. A point of critique would be, that you
+can’t use command like @me in the filter context. Nonetheless, the documentation is spot on, right beneath
+the search of filter function all possibilities are explained.
+
+<h2>Integrations and powerups</h2>
+
+To be frank I did not use too many Powerups since the tool already has so many things baked in. But there is
+a plethora (fancy word that makes me look smart) of things you can add to a board to add functionality
+you might miss. A prominent example is a calendar to show all your due dates, but there is so much more. I
+recently used a <a href="https://info.trello.com/power-ups/card-aging">Powerup that indicated the <em>age
+    </em>of cards</a> by making them more transparent when there was no activity for a longer period.
+You also can integrate almost any service, add custom fields, repeat actions <a href="https://trello.com/power-ups">and
+    so much more</a>.
+
+<img
+    src="https://res.cloudinary.com/simonvomeyser/image/upload/v1526615630/Trello/Bildschirmfoto_2018-05-18_um_05.52.40.png">
+
+<h2>It’s the little things</h2>
+There are so much more little features that make me smile every day and that are the main reason I wrote this
+article.
+
+You can drag the avatars of the boards members onto cards
+
+<img src="https://res.cloudinary.com/simonvomeyser/image/upload/v1529040916/Trello/Kapture_2018-06-15_at_7.29.36.gif">
+
+You can start writing the title of a new card in one list, and then finish it in another
+
+<img src="https://res.cloudinary.com/simonvomeyser/image/upload/v1529041166/Trello/Kapture_2018-06-15_at_7.39.14.gif">
+
+Use things like @name, #label or ^position while creating cards to assign people, labels or position a card
+during creation. Details are at the bottom of the <a href="https://trello.com/shortcuts">shortcuts page</a>.
+
+
+<img src="https://res.cloudinary.com/simonvomeyser/image/upload/v1532408770/Trello/Kapture_2018-07-24_at_7.04.29.gif">
+
+The color of your board or the choosen background will by shown as favicon in your browser tab for
+orientation
+
+<img src="https://res.cloudinary.com/simonvomeyser/image/upload/v1532409222/Trello/Kapture_2018-07-24_at_7.12.16.gif">
+
+You can use stickers and attach them to cards to make everything more colorful
+
+<img
+    src="https://res.cloudinary.com/simonvomeyser/image/upload/q_69/v1532409834/Trello/Kapture_2018-07-24_at_7.22.03.gif">
+
+And just to utter a little critique: How to alter or remove stickers is something, the UX team (in my
+opinion) has not found the perfect solution for. Here is how 🙂
+
+<img
+    src="https://res.cloudinary.com/simonvomeyser/image/upload/q_67/v1532409933/Trello/Kapture_2018-07-24_at_7.23.19.gif">
+
+<h2>Last words</h2>
+That’s a lot right? And just to be clear, I am not saying trello is the best tool for every purpose in
+project management. But most tools, while clearly having advantages for certain use cases, lack these
+simple features I would consider immensely helpful. Why isn’t my todolist app able to keyboard navigate, why
+is the project managend tool at a company I work for not auto saving my comments draft when I am
+accidentally hit the escape key? And why am I trying to apologize to people who will get angry no matter
+what because their grandma programmed JIRA?
+
+I am just trying to be helpful and have a little fun. Let’s all be friends and hug each other more often,
+like the Trello UX team does with it’s users.
+
+
+@endsection
