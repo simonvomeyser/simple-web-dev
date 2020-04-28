a6c7a707b1d1d854432879ecc6c168ef56723166
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-04-28 12:06:53 +0200

    Delete test posts

--- resources/views/posts/post-one-about-something.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Post one about something')
-
-@section('content')
-    <p>post-one-about-something.blade.php</p>
-@endsection
- 
--- resources/views/posts/post-two-about-another-thing.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Post Two about a few things')
-
-@section('content')
-    <p>post-two-about-another-thing.php</p>
-@endsection
