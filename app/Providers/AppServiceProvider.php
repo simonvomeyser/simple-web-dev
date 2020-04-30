4efb8b9675c752572154fae7a2266b42152a3aef
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-04-30 19:44:55 +0200

    Add helper function to sort by date

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\FactoryProxy;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Factory as EloquentFactory;
 
@@ -27,6 +28,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Collection::macro('sortByDate', function ($column = 'created_at', $order = SORT_DESC) {
+            return $this->sortBy(function ($date) use ($column) {
+                return strtotime($date->$column);
+            }, SORT_REGULAR, $order == SORT_DESC);
+        });
     }
 }
