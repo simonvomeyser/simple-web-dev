ffb6e5e48d240ff437ff6991a12f5bdb4fea83be
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-01 14:01:25 +0200

    Rename and work with case sensitivity

--- tests/Feature/PostsPageTest.php
+++ tests/Feature/PostsPageTest.php
@@ -4,15 +4,13 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Support\Carbon;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class FrontPageTest extends TestCase
+class PostsPageTest extends TestCase
 {
     /** @test */
-    public function the_frontpage_is_reachable()
+    public function the_page_is_reachable()
     {
-        $response = $this->get('/');
+        $response = $this->get(route('posts'));
 
         $response->assertOk();
     }
@@ -20,42 +18,33 @@ class FrontPageTest extends TestCase
     /** @test */
     public function the_page_shows_no_post_if_none_are_present()
     {
-        $this->get('/')->assertSee(__('posts.no-posts-found'));
+        $this->get(route('posts'))->assertSee(__('posts.no-posts-found'));
     }
 
     /** @test */
-    public function three_created_posts_are_found_with_essential_data()
+    public function posts_are_found_with_essential_data()
     {
-        $posts = factory('App\Post', 3)->create();
+        $posts = factory('App\Post', 6)->create();
 
-        $response = $this->get('/');
+        $response = $this->get(route('posts'));
 
         foreach ($posts as $post) {
             $response
                 ->assertSee($post->title)
+                ->assertSee($post->link())
                 ->assertSee($post->excerpt)
                 ->assertSee($post->list_header_image);
         }
     }
 
     /** @test */
-    public function only_three_posts_are_passed_to_the_view()
-    {
-        factory('App\Post', 5)->create();
-
-        $this->get('/')->assertViewHas('posts', function ($posts) {
-            return $posts->count() < 4;
-        });
-    }
-
-    /** @test */
-    public function only_released_posts_are_not_shown()
+    public function only_released_posts_are_shown()
     {
         $postWithoutReleaseDate = factory('App\Post')->create(['release_date' => '']);
         $postWithFutureReleaseDate = factory('App\Post')->create(['release_date' => Carbon::tomorrow()]);
         $postWithReleaseDate = factory('App\Post')->create();
 
-        $this->get('/')
+        $this->get(route('posts'))
             ->assertSee($postWithReleaseDate->link())
             ->assertDontSee($postWithoutReleaseDate->link())
             ->assertDontSee($postWithFutureReleaseDate->link());
