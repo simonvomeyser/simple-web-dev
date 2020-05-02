08c1c06fa1b1d25aa0de186beb768fa83002a442
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-02 18:14:42 +0200

    Format, add optional title

--- resources/views/components/video.blade.php
+++ resources/views/components/video.blade.php
@@ -1,7 +1,8 @@
 @props(['code', 'title'])
 
 <figure class="video">
-    <iframe title="{{$title}}" src="https://www.youtube.com/embed/{{$code}}?feature=oembed" frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="">
+    <iframe @if($title ?? null) title="{{$title}}" @endif src="https://www.youtube.com/embed/{{$code}}?feature=oembed"
+        frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen="">
     </iframe>
 </figure>
