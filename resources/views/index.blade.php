98d0f83425a36299c20822508234c323cfcbde2e
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-04 15:50:00 +0200

    Add container

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,22 @@
 @extends('layouts.app')
 
 @section('main')
-<p>Welcome, here are your blade posts</p>
+<div class="container">
 
-{{count($posts)}}
-@forelse ($posts as $item)
-<div>
-    <a href="{{ $item->link() }}">
-        <img src="{{$item->list_header_image}}" alt="List image of the post {{$item->title}}">
-    </a>
-    <div>{{$item->title}}</div>
-    <div>{{$item->excerpt}}</div>
+    <p>Welcome, here are your blade posts</p>
+
+    @forelse ($posts as $item)
+    <div>
+        <a href="{{ $item->link() }}">
+            <img src="{{$item->list_header_image}}" alt="List image of the post {{$item->title}}">
+        </a>
+        <div>{{$item->title}}</div>
+        <div>{{$item->excerpt}}</div>
+    </div>
+    @empty
+    <p>{{__('posts.no-posts-found')}}</p>
+    @endforelse
 </div>
-@empty
-<p>{{__('posts.no-posts-found')}}</p>
-@endforelse
+
 
 @endsection
