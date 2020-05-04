45b3f0b273bb381d42723c0c1476b7e43c1249cf
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-04 16:19:56 +0200

    Add basic posts list layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,47 @@
 @extends('layouts.app')
 
 @section('main')
-<div class="container">
+<section class="container">
 
-    <p>Welcome, here are your blade posts</p>
+    <h1>The 3 most recent posts:</h1>
 
-    @forelse ($posts as $item)
-    <div>
-        <a href="{{ $item->link() }}">
-            <img src="{{$item->list_header_image}}" alt="List image of the post {{$item->title}}">
-        </a>
-        <div>{{$item->title}}</div>
-        <div>{{$item->excerpt}}</div>
+    <div class="posts">
+        @forelse ($posts as $item)
+        <article class="post-preview">
+            <div class="post-preview__thumbnail">
+                <a href="{{ $item->link() }}">
+                    <img src="{{$item->list_header_image}}" alt="List image of the post {{$item->title}}">
+                </a>
+            </div>
+            <div class="post-preview__under-image">
+                <div class="post-preview__meta">
+                    <div class="post-preview__categories">
+                        todo: categories
+                    </div>
+                    <div class="post-preview__est-reading-time">
+                        todo: reading time
+                    </div>
+                </div>
+                <div class="post-preview__heading">
+                    <h2>{{$item->title}}</h2>
+                </div>
+                <div class="post-preview__excerpt">
+                    <div>{{$item->excerpt}}</div>
+                </div>
+                <div class="post-preview__button">
+                    <a href="{{$item->link()}}" class="button">Read more</a>
+                </div>
+
+            </div>
+
+        </article>
+        @empty
+        <div class="no-posts">
+            <img src="{{asset('images/found-nothing.svg')}}" alt="Oh no, a sad looking doggo">
+        </div>
+        @endforelse
     </div>
-    @empty
-    <div class="no-posts">
-        <img src="{{asset('images/found-nothing.svg')}}" alt="Oh no, a sad looking doggo">
-    </div>
-    @endforelse
-</div>
+</section>
 
 
 @endsection
