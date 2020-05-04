1c2f9526d4d1d0106973165858229af0e93f76bf
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-04 15:51:10 +0200

    Add stylesheet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,18 +10,8 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-    </style>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+
 </head>
 
 <body>
