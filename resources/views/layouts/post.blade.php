c578c64264a79e04f96ba4276dba71072fb94c29
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-04 15:52:49 +0200

    Add narrow container to layout

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('main')
-<h1> @yield('title') </h1>
-<img src="@yield('header_image')" alt="">
-<x-post-content>
-    @yield('content')
-</x-post-content>
+<div class="container container--narrow">
+
+    <h1> @yield('title') </h1>
+    <img src="@yield('header_image')" alt="">
+    <x-post-content>
+        @yield('content')
+    </x-post-content>
+</div>
+
 @endsection
