7dd14d2c07f49b487ebc86f7aaa22dc351097ffe
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-04 16:37:34 +0200

    Add title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700|Sanchez" rel="stylesheet">
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
+    <title>Simple Web Dev - A blog about webdevelopment</title>
 
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
 
