f1641f5119c42bda65b2487f9b168e96a752194d
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-04 16:04:01 +0200

    Add basic posts page markup and todo search

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,18 +1,38 @@
 @extends('layouts.app')
 
 @section('main')
-<p>Welcome, here are all your blade posts</p>
+<section class="container">
+    <form action="todo" method="get" class="search-inline-form">
+        <div class="search-inline-form__inner">
+            <span class="search-inline-form__label-background"></span>
+            <label for="search-input">Search</label>
+            <input type="text" value="?" name="s" id="search-input"><button type="submit" class="button">find</button>
+        </div>
+    </form>
+    <div class="search-result-info">
+        Found ??
+    </div>
+    <div class="search-result-info">
+        Found ??
+    </div>
+</section>
+<section class="container">
 
-@forelse ($posts as $item)
-<div>
-    <a href="{{ $item->link() }}">
-        <img src="{{$item->list_header_image}}" alt="List image of the post {{$item->title}}">
-    </a>
-    <div>{{$item->title}}</div>
-    <div>{{$item->excerpt}}</div>
-</div>
-@empty
-<p>{{__('posts.no-posts-found')}}</p>
-@endforelse
+    <p>Welcome, here are all your blade posts</p>
+
+    @forelse ($posts as $item)
+    <div>
+        <a href="{{ $item->link() }}">
+            <img src="{{$item->list_header_image}}" alt="List image of the post {{$item->title}}">
+        </a>
+        <div>{{$item->title}}</div>
+        <div>{{$item->excerpt}}</div>
+    </div>
+    @empty
+    <p>{{__('posts.no-posts-found')}}</p>
+    @endforelse
+
+    </div>
+</section>
 
 @endsection
