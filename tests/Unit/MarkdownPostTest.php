fd0e6a8739592fd3509d3ecbc0ccd9e2f689531d
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-05 19:00:07 +0200

    Add test skeleton

---
+++ tests/Unit/MarkdownPostTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MarkdownPostTest extends TestCase
+{
+    use WithFaker;
+
+    /** @test */
+    public function a_post_can_be_created_from_a_markdown_file()
+    {
+    }
+
+    /** @test */
+    public function it_parses_the_frontmatter_data_in_the_right_datatypes()
+    {
+    }
+
+    /** @test */
+    public function it_returns_a_link_to_itself()
+    {
+    }
+
+    /** @test */
+    public function only_released_posts_are_returned_from_released_scope()
+    {
+    }
+
+    /** @test */
+    public function all_posts_are_returned_from_released_scope_when_in_local_development_mode()
+    {
+    }
+
+    /** @test */
+    public function it_returns_the_right_instance_classname()
+    {
+    }
+
+    /** @test */
+    public function released_posts_are_sorted_by_release_date()
+    {
+    }
+
+    /** @test */
+    public function posts_can_be_found_by_their_filename_slug()
+    {
+    }
+
+    /** @test */
+    public function posts_can_also_be_found_by_their_definded_slug()
+    {
+    }
+
+    /** @test */
+    public function a_post_can_be_checked_if_it_exists()
+    {
+    }
+
+    /** @test */
+    public function a_view_response_can_be_retrieved_from_an_existing_blade_based_model()
+    {
+    }
+
+    /** @test */
+    public function a_blade_based_model_is_responsable()
+    {
+    }
+
+    /** @test */
+    public function a_not_existent_blade_based_model_throws404()
+    {
+    }
+
+    /** @test */
+    public function it_estimates_reading_time()
+    {
+    }
+}
