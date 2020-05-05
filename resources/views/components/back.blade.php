f432517c91f6c2d7717db64a2314820665bef5e4
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-05 11:42:29 +0200

    Add really basic back button implementation

---
+++ resources/views/components/back.blade.php
@@ -0,0 +1,4 @@
+<a href="#" class="back" onclick="history.back()">
+    <img src="{{asset('images/left-arrow.svg') }}" alt="An arrow pointing to the left">
+    <div class="back__text">Back</div>
+</a>
