0b380b9bc3d1962fef919c08362bd2bcc0525de0
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-08 20:04:48 +0200

    Complete frontpage test rewrite

--- tests/Feature/FrontPageTest.php
+++ tests/Feature/FrontPageTest.php
@@ -45,41 +45,19 @@ class FrontPageTest extends TestCase
     /** @test */
     public function only_three_posts_are_passed_to_the_view()
     {
-        factory('App\Post', 5)->create();
+        MarkdownPost::fake();
 
         $this->get('/')->assertViewHas('posts', function ($posts) {
             return $posts->count() < 4;
         });
     }
 
-    /** @test */
-    public function only_released_posts_are_not_shown()
+    public function only_released_posts_are_shown()
     {
-        $postWithoutReleaseDate = factory('App\Post')->create(['release_date' => '']);
-        $postWithFutureReleaseDate = factory('App\Post')->create(['release_date' => Carbon::tomorrow()]);
-        $postWithReleaseDate = factory('App\Post')->create();
+        MarkdownPost::fake();
 
-        $this->get('/')
-            ->assertSee($postWithReleaseDate->link())
-            ->assertDontSee($postWithoutReleaseDate->link())
-            ->assertDontSee($postWithFutureReleaseDate->link());
-    }
-
-
-    /** @test */
-    public function the_three_latest_posts_are_found()
-    {
-        factory('App\Post', 3)->create(['release_date' => Carbon::now()->subMonth()]);
-        $currentPosts = factory('App\Post', 3)->create(['release_date' => Carbon::yesterday()]);
-        factory('App\Post', 3)->create(['release_date' => Carbon::now()->subYear()]);
-
-        $response = $this->get('/');
-
-        foreach ($currentPosts as $post) {
-            $response
-                ->assertSee($post->title)
-                ->assertSee($post->excerpt)
-                ->assertSee($post->list_header_image);
-        }
+        $this->get('/')->assertViewHas('posts', function ($posts) {
+            return $posts->isReleased();
+        });
     }
 }
