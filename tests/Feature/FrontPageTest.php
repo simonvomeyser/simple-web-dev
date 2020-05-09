90860364fdef01f5167621e19ab5ea110452f39a
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-09 14:09:45 +0200

    Cleanup

--- tests/Feature/FrontPageTest.php
+++ tests/Feature/FrontPageTest.php
@@ -3,9 +3,6 @@
 namespace Tests\Feature;
 
 use App\Markdown\MarkdownPost;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Carbon;
 use Tests\TestCase;
 
 class FrontPageTest extends TestCase
