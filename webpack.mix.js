600f2d51e5532952415b33d9bae88b47dc6df387
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-12 11:36:28 +0200

    Add md to reloading

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,12 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .browserSync(process.env.APP_URL)
+    .browserSync({
+        proxy: process.env.APP_URL,
+        files: [
+            'app/**/*.php',
+            'resources/views/**/*.php',
+            'resources/**/*.md',
+            `${Config.publicPath || 'public'}/**/*.(js|css)`
+        ],
+    })
