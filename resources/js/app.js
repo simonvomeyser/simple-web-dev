f72f3d4dd00bef82d0caea2ad564902b0786bb79
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-18 14:27:40 +0200

    Improve BEM helpers

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import EmbedPlayground from './components/EmbedPlayground.vue';
 import EmbedVideo from './components/EmbedVideo.vue';
+import Sidenote from './components/Sidenote.vue';
 
 new Vue({
     el: '#app',
     components: {
         EmbedVideo,
         EmbedPlayground,
+        Sidenote,
     }
 });
 
---
+++ resources/js/components/Sidenote.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sidenote">
+    <div class="sidenote__heading">{{heading}}</div>
+    <div class="sidenote__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    heading: String
+  }
+};
+</script>
