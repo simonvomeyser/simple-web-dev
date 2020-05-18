9d6e1a7abeecf286a650b5dbfb7f68c76e9efcf9
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-18 11:34:06 +0200

    Disable mix notification

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.disableNotifications();
+
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .browserSync({
         proxy: process.env.APP_URL,
+        notify: false,
         files: [
             'app/**/*.php',
             'resources/views/**/*.php',
