4c3167dab13b9e6f43ad3f34798ea71d0998d76e
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-05-26 12:27:31 +0200

    Fix post styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700|Sanchez" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700|Sanchez" rel="stylesheet">
 
     <title>Simple Web Dev - A blog about webdevelopment</title>
 
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,30 +9,16 @@
 <div class="container container--narrow">
 
     <div class="post">
-        <div class="post__back">
-            @include('includes.back')
-        </div>
         <div class="post__meta">
             <div class="post-meta">
-                <div class="post-meta__left">
-                    <div class="post-meta__heading">
-                        <h1> {{$post->title}} </h1>
-                    </div>
-                    <div class="post-meta__beneath-heading">
-                        @each('includes.tag-pill', $post->tags, 'tag')
-                    </div>
+                <div class="post-meta__heading">
+                    <h1> {{$post->title}} </h1>
                 </div>
-                <div class="post-meta__right">
-                    <div class="post-meta__author-image">
-                        <img src="http://0.gravatar.com/avatar/cd029fe67e1a8a52e55bbed957848351?s=96&d=mm&r=g"
-                            alt="Image of Simon trying to look professional">
-                    </div>
-                    <div class="post-meta__author">
-                        written by Simon <br>
-                    </div>
-                    <div class="post-meta__date">
-                        <div>{{$post->getReadableRelease()}}</div>
-                    </div>
+                <div class="post-meta__date">
+                    Written by Simon {{$post->getReadableRelease()}}
+                </div>
+                <div class="post-meta__tags">
+                    @each('includes.tag-pill', $post->tags, 'tag')
                 </div>
             </div>
         </div>
