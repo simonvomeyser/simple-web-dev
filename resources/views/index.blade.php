311dc72722fbd722b601c5c320120d6f30f0cf52
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-06-02 10:48:25 +0200

    Add search to index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,22 @@
 
 @section('main')
 <section class="container">
+    <form action="todo" method="get" class="search-inline-form">
+        <div class="search-inline-form__inner">
+            <span class="search-inline-form__label-background"></span>
+            <label for="search-input">Search</label>
+            <input type="text" value="?" name="s" id="search-input"><button type="submit" class="button">find</button>
+        </div>
+    </form>
+    <div class="search-result-info">
+        Found ??
+    </div>
+    <div class="search-result-info">
+        Found ??
+    </div>
+</section>
+
+<section class="container">
 
     @if ($posts->count())
 
