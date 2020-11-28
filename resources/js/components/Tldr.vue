4fa158375443d3f44b9205af2df96730df27a640
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-11-28 14:32:59 +0100

    Drafting

--- resources/js/components/Tldr.vue
+++ resources/js/components/Tldr.vue
@@ -2,10 +2,9 @@
     <div class="tldr">
 
         <div class="tldr__trigger" v-if="!hideButton">
-            <button class="button button--primary button--well-fed" @click="openUp"><span v-if="!open">TLDR:</span>&nbsp;{{ computedButtonText }}
-            </button>
+            <button class="button button--secondary button--well-fed" @click="openUp">TL;DR?</button>
         </div>
-        <div class="tldr__content"  id="tldr" v-show="open">
+        <div class="tldr__content" id="tldr" v-show="open">
             <h2>TL; DR</h2>
             <slot></slot>
         </div>
@@ -14,30 +13,16 @@
 
 <script>
 
-const buttonTextJokes = [
-    'In a hurry?',
-    'Too much text?',
-    'Just tell me what do do!',
-    'No time to read?',
-    'No time to read?',
-];
-
 export default {
     props: {
         buttonText: {
             type: String,
-            default: buttonTextJokes[Math.floor(Math.random() * buttonTextJokes.length)]
         }
     },
     data() {
         return {
             open: false,
             hideButton: false
-        }
-    },
-    computed: {
-        computedButtonText() {
-            return this.open ? 'Okay okay!' : this.buttonText
         }
     },
     mounted() {
@@ -51,7 +36,7 @@ export default {
             setTimeout(() => {
                 this.hideButton = true;
                 location.hash = "tldr";
-            }, 1000)
+            }, 250)
         }
     }
 };
