f181ac6e6c1b384852c8f6561eb8efeec7cd0a66
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-12-13 18:19:34 +0100

    Add mvp of tldr modal

--- resources/js/components/Tldr.vue
+++ resources/js/components/Tldr.vue
@@ -1,19 +1,24 @@
 <template>
-    <div class="tldr">
+    <div class="tldr" data-close>
         <div class="tldr__info" :class="{'tldr__info--shown' : showInfo}">
             <div class="tldr-info">
                 <div class="tldr-info__heading">In a hurry?</div>
                 <div class="tldr-info__copy">This post <br> has a short summary!</div>
                 <div class="tldr-info__button">
-                    <button class="button button--understatement">
+                    <button class="button button--understatement" @click="open = true">
                         TLDR
                     </button>
                 </div>
             </div>
         </div>
-        <div class="tldr__content" id="tldr" v-show="open">
-            <h2>Summary</h2>
-            <slot></slot>
+        <div class="tldr__modal" v-show="open">
+            <div class="tldr-modal">
+                <div class="tldr-modal__content">
+                    <button class="tldr-modal__close" @click="open = false">&times;</button>
+                    <h2>Summary</h2>
+                    <slot></slot>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -40,6 +45,7 @@ export default {
         });
         this.handleDebouncedScroll = debounce(this.handleScroll, 100);
         window.addEventListener('scroll', this.handleDebouncedScroll);
+        window.addEventListener('keydown', this.handleKeydown);
     },
     methods: {
         openUp() {
@@ -48,8 +54,14 @@ export default {
         handleScroll() {
             this.showInfo = (window.scrollY > 50 && window.scrollY < 1000 );
         },
+        handleKeydown(e) {
+            if(e.key === "Escape") {
+               this.open = false;
+            }
+        },
         beforeDestroy() {
             window.removeEventListener('scroll', this.handleDebouncedScroll);
+            window.removeEventListener('keydown', this.handleKeydown);
         }
     }
 };
