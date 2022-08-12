2e2ec806810ce91231db7ec50db2d08ba4faae27
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-08-12 09:05:00 +0200

    feat: return api resource

---
+++ app/Http/Resources/MarkdownPostApiResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MarkdownPostApiResource extends JsonResource
+{
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'excerpt' =>  $this->excerpt->toHtml(),
+            'tags' => $this->tags,
+            'list_image' => $this->list_image,
+            'slug' => $this->slug,
+            'link' => $this->getLink()
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Resources\MarkdownPostApiResource;
 use App\Markdown\MarkdownPost;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,5 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/recent-posts', function() {
-  return  MarkdownPost::released()->take(2);
+  return  MarkdownPostApiResource::collection(MarkdownPost::released()->take(2));
 });
