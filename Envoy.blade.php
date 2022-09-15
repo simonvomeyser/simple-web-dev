52c2cd4fe09d67de577a1c855ec9ac7ea93a215b
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-09-15 18:36:40 +0200

    feat: add ability to deploy with laravel envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,20 @@
+@setup
+    require __DIR__.'/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+@endsetup
+
+@servers(['web' => [env('DEPLOY_USER') . '@' . env('DEPLOY_HOST')]])
+
+@task('deploy', ['on' => 'web'])
+    source {{ env('DEPLOY_LOCATION') }}/../.bashrc
+    cd {{ env('DEPLOY_LOCATION') }}
+    git pull
+    npm install
+    npm run prod
+    php81 /usr/bin/composer install
+    php81 artisan cache:clear
+    php81 artisan config:clear
+    php81 artisan view:clear
+    php81 artisan cache-posts
+@endtask
